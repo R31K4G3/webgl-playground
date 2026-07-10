b4559f6dc2ef546e8ab229eb74d2fa703d44f437
れ 〜 か げ <125360100+R31K4G3@users.noreply.github.com>
2026-07-10 23:19:55 +0900

    Clear log before compiling

--- script.js
+++ script.js
@@ -160,6 +160,7 @@ function main() {
     gl.createVertexShader("precision mediump float;attribute vec2 position;void main(void){gl_Position=vec4(position,0.0,1.0);}");
 
     function compileAndDraw() {
+        glLog.textContent = "";
         gl.createFragmentShader(fsSource.value);
         gl.compileProgram();
 
